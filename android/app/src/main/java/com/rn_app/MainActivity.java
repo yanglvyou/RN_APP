e2856c889b32d3f6301a94de4705ffb8cd386bd6
lvyou <376607092@qq.com>
2020-08-02 22:23:20 +0800

    android 配置SplashScreen

--- android/app/src/main/java/com/rn_app/MainActivity.java
+++ android/app/src/main/java/com/rn_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.rn_app;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,13 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "RN_APP";
+  }
+
+  // 重写onCreate方法，整个RN项目的加载的入口
+  @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      // 显示启动屏，第二个参数是我们自定义主题的引用
+      SplashScreen.show(this, R.style.SplashScreenTheme);
+      super.onCreate(savedInstanceState);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {Provider as StoreProvider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 import Navigator from '@/navigator/index';
 import store from '@/config/dva';
 
@@ -8,6 +9,9 @@ import store from '@/config/dva';
 const Drawer = createDrawerNavigator();
 
 const App = () => {
+  React.useEffect(()=>{
+    SplashScreen.hide();
+  })
   return (
     <StoreProvider store={store}>
       <Navigator />
