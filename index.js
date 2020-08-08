62959422f351387dd8f022c884c7f717c4c80379
lvyou <376607092@qq.com>
2020-08-08 12:59:38 +0800

    设置android app名称

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
  * @format
  */
 
-import {AppRegistry,YellowBox} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import App from './src/index';
 import {name as appName} from './app.json';
 
+if (!__DEV__) {
+  const emptyFunc = () => {};
+  global.console.info = emptyFunc;
+  global.console.log = emptyFunc;
+  global.console.warn = emptyFunc;
+  global.console.error = emptyFunc;
+}
+
 YellowBox.ignoreWarnings([
-    'Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`',
-    `Can't perform a React state update on an unmounted component.`,
-    'FlatList: Calling `getNode()` on the ref of an Animated component is no longer necessary.',
-    `Can't perform a React state update on an unmounted component`,
-    'source.uri should not be an empty string',
-  ])
+  'Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`',
+  `Can't perform a React state update on an unmounted component.`,
+  'FlatList: Calling `getNode()` on the ref of an Animated component is no longer necessary.',
+  `Can't perform a React state update on an unmounted component`,
+  'source.uri should not be an empty string',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
